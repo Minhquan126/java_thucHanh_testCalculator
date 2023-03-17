cb537466a8eb432806c3356c90ff0f2fa40f87d2
unknown <minhquanctm@gmail.com>
2023-03-17 14:21:15 +0700

    a

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calculator {
+    public static int add(int firstNumber, int secondNumber){
+        throw new UnsupportedOperationException();
+    }
+    public static int sub(int firstNumber, int secondNumber){
+        throw new UnsupportedOperationException();
+    }
+}
